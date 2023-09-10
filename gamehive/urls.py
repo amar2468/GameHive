86275d074205ec16f918c907589a5357f2f9836e
amar2468 <plakaloam@gmail.com>
2023-09-10 11:33:32 +0100

    Imported the views file

--- gamehive/urls.py
+++ gamehive/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('guess_the_digit/', include('guess_the_digit.urls'))
+    path('guess_the_digit/', include('guess_the_digit.urls')),
+    path('', views.homepage)
 ]
