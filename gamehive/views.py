b66d608ec6ec715e803214e3be82e519892cdbd3
amar2468 <plakaloam@gmail.com>
2023-09-10 18:07:10 +0100

    About page and contact page views have been created

--- gamehive/views.py
+++ gamehive/views.py
@@ -4,6 +4,12 @@ from .forms import RegistrationForm
 def homepage(request):
     return render(request, 'index.html')
 
+def about(request):
+    return render(request, 'about.html')
+
+def contact_page(request):
+    return render(request, 'contact.html')
+
 def sign_up(request):
     if request.method == "POST":
         form = RegistrationForm(request.POST)
