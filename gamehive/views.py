4802f8b0f2cded9076cece2c96700cde107f8c8e
amar2468 <plakaloam@gmail.com>
2023-09-18 15:52:13 +0100

    Added functionality to save the user information into a database & added login functionality that will allow a user to login after creating account

--- gamehive/views.py
+++ gamehive/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 def homepage(request):
     return render(request, 'index.html')
@@ -15,8 +17,30 @@ def sign_up(request):
         form = RegistrationForm(request.POST)
 
         if form.is_valid():
-            return redirect('')
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            User.objects.create_user(username=username, email=email, password=password)
+
+            return render(request, 'login.html')
+        if not form.is_valid():
+            print(form.errors)
     else:
         form = RegistrationForm()
     return render(request, 'sign_up.html', {'form': form})
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return render(request, 'index.html')
+        else:
+            return render(request, 'about.html')
+    return render(request, 'login.html')
         
