cb4263160f57d34421bd5ef051f89e43b2b885b4
amar2468 <plakaloam@gmail.com>
2023-09-18 15:52:50 +0100

    Added the "required" attribute to all form fields

--- gamehive/forms.py
+++ gamehive/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class RegistrationForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    password = forms.CharField(widget=forms.PasswordInput(), required=True)
