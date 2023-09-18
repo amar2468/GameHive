eefa2deee16babf1f79d901e8829f54b8ce6083f
amar2468 <plakaloam@gmail.com>
2023-09-18 16:48:29 +0100

    Added unit tests to handle incorrect email entries, such as not entering domain name

--- gamehive/tests.py
+++ gamehive/tests.py
@@ -66,3 +66,102 @@ class RegistrationTestCase(TestCase):
 
         self.assertIn('password', form.errors)
         self.assertEqual(form.errors['password'], ['This field is required.'])
+    
+    def testing_incorrect_email_field(self):
+        # Testing email without .com part
+
+        data = {
+            'username': 'testuser',
+            'email': 'amar@gmail',
+            'password': 'Strongpassword12!'
+        }
+
+        form = RegistrationForm(data=data)
+
+        self.assertFalse(form.is_valid())
+
+        # Testing email without gmail.com part
+
+
+        data = {
+            'username': 'testuser',
+            'email': 'amar@',
+            'password': 'Strongpassword12!'
+        }
+
+        form = RegistrationForm(data=data)
+
+        self.assertFalse(form.is_valid())
+
+        # Testing email with local name but without domain name
+
+        data = {
+            'username': 'testuser',
+            'email': 'amar@',
+            'password': 'Strongpassword12!'
+        }
+
+        form = RegistrationForm(data=data)
+
+        self.assertFalse(form.is_valid())
+
+        # Testing email with weird characters
+
+        data = {
+            'username': 'testuser',
+            'email': '&"*"*"@gmail.com',
+            'password': 'Strongpassword12!'
+        }
+
+        form = RegistrationForm(data=data)
+
+        self.assertFalse(form.is_valid())
+
+        # Test missing @gmail part
+
+        data = {
+            'username': 'testuser',
+            'email': 'amar.com',
+            'password': 'Strongpassword12!'
+        }
+
+        form = RegistrationForm(data=data)
+
+        self.assertFalse(form.is_valid())
+
+        # Testing spaces in email
+
+        data = {
+            'username': 'testuser',
+            'email': 'amar pla@gmail.com',
+            'password': 'Strongpassword12!'
+        }
+
+        form = RegistrationForm(data=data)
+
+        self.assertFalse(form.is_valid())
+
+        # Excessive length emails
+
+        data = {
+            'username': 'testuser',
+            'email': 'a'*1000 + '@' + 'b'*1000 + '.com',
+            'password': 'Strongpassword12!'
+        }
+
+        form = RegistrationForm(data=data)
+
+        self.assertFalse(form.is_valid())
+
+        # Testing non-ASCII email
+
+        data = {
+            'username': 'testuser',
+            'email': 'юзер@example.com',
+            'password': 'Strongpassword12!'
+        }
+
+        form = RegistrationForm(data=data)
+
+        self.assertFalse(form.is_valid())
+        
