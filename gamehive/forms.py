1c20fe8c145f7a0f8e1f87fa40100dbaea610d2f
amar2468 <plakaloam@gmail.com>
2023-09-19 14:26:35 +0100

    Added validation for the username

--- gamehive/forms.py
+++ gamehive/forms.py
@@ -1,6 +1,24 @@
 from django import forms
+from django.core.exceptions import ValidationError
+import re
+
+MIN_USERNAME_LENGTH = 3
+MAX_USERNAME_LENGTH = 20
+RESERVED_WORDS = ['admin', 'root', 'moderator']
+
+def validate_username(username):
+    if len(username) < MIN_USERNAME_LENGTH:
+        raise ValidationError(f"Username must be at least {MIN_USERNAME_LENGTH} characters long.")
+    if len(username) > MAX_USERNAME_LENGTH:
+        raise ValidationError(f"Username must be no longer than {MAX_USERNAME_LENGTH} characters.")
+    if ' ' in username or any(char.isspace() for char in username):
+        raise ValidationError("Username cannot contain spaces.")
+    if not re.match(r'^[a-zA-Z0-9]*$', username):
+        raise ValidationError("Username cannot contain special characters.")
+    if username.lower() in RESERVED_WORDS:
+        raise ValidationError("Username is not allowed.")
 
 class RegistrationForm(forms.Form):
-    username = forms.CharField(max_length=25, required=True)
+    username = forms.CharField(max_length=25, required=True, validators=[validate_username])
     email = forms.EmailField(required=True)
     password = forms.CharField(widget=forms.PasswordInput(), required=True)
