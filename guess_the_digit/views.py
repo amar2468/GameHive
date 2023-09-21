517f6ba755a57fc1a2f4330d7068aa9bcb644960
amar2468 <plakaloam@gmail.com>
2023-09-21 17:12:08 +0100

    Started coding the guessing game functionality, with the basics completed. More will follow.

--- guess_the_digit/views.py
+++ guess_the_digit/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+import random
 
-# Create your views here.
 
-def check_if_works(request):
+guess = 3
+
+# Guess the digit game functionality in the view below
+
+@login_required
+def guess_the_digit_game(request):
+    global guess
     if request.method == "POST":
 
         user_guess = int(request.POST.get('guess_number_input_field', ''))
 
+        correct_number = request.session.get('correct_number')
+
+        result = ""
+
+        if user_guess == correct_number:
+            result = "Correct guess! Well done!"
+        elif user_guess != correct_number and guess == 0:
+            result = "Game over! The correct number was " + str(correct_number)
+            request.session.pop('correct_number', None)
+        elif user_guess != correct_number and guess != 0:
+            result = "Wrong guess! Try again!"
+            guess -= 1
+        
         context = {
-            'result': 'You guessed ' + str(user_guess) + '! Congratulations!'
+            'result': result
         }
 
         return render(request, 'home.html', context)
+    
+    if 'correct_number' not in request.session:
+        request.session['correct_number'] = random.randint(1,10)
     return render(request, 'home.html')
