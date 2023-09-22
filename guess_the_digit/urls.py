6a0044404d16f5c619394121b22ef9e13e854c0d
amar2468 <plakaloam@gmail.com>
2023-09-22 16:10:29 +0100

    Added config url, that will lead the user to the configuration part of the game

--- guess_the_digit/urls.py
+++ guess_the_digit/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('play/', views.guess_the_digit_game, name='play')
+    path('play/', views.guess_the_digit_game, name='play'),
+    path('config/', views.guess_the_digit_config, name='config')
 ]
