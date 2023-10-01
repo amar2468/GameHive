eb1917cfa11ca5279298223599db74d8b14f8343
amar2468 <plakaloam@gmail.com>
2023-10-01 19:03:18 +0100

    Changed the "number of guesses" by hard coding the number of guesses, depending on the level.

--- guess_the_digit/views.py
+++ guess_the_digit/views.py
@@ -45,14 +45,6 @@ def guess_the_digit_game(request):
             else:
                 result = "Game over! The correct number was " + str(correct_number)
                 request.session.pop('correct_number', None)
-
-                if 'score' in request.session:
-                    if level == "easy":
-                        request.session['score'] -= 10
-                    elif level == "medium":
-                        request.session['score'] -= 50
-                    elif level == "hard":
-                        request.session['score'] -= 100
         
         # The result is stored in the context so it can be returned back to the html template
         context = {
@@ -63,7 +55,6 @@ def guess_the_digit_game(request):
 
         return render(request, 'home.html', context)
     
-    number_of_guesses = int(request.GET.get('number_of_guesses', ''))
     level = request.GET.get('selected_level')
 
     # Check if the correct number has been specified in the current session
@@ -72,14 +63,17 @@ def guess_the_digit_game(request):
         # If the level is easy, generate a random number between 1 and 10
         if level == 'easy':
             request.session['correct_number'] = random.randint(1,10)
+            number_of_guesses = 5
         
         # If the level is medium, generate a random number between 1 and 50
         elif level == 'medium':
             request.session['correct_number'] = random.randint(1,50)
+            number_of_guesses = 10
 
         # If the level is hard, generate a random number between 1 and 100
         elif level == 'hard':
             request.session['correct_number'] = random.randint(1,100)
+            number_of_guesses = 20
     # If the correct number has already been specified, it needs to be re-generated
     if 'correct_number' in request.session:
 
@@ -87,16 +81,19 @@ def guess_the_digit_game(request):
         if level == 'easy':
             request.session.pop('correct_number', None)
             request.session['correct_number'] = random.randint(1,10)
+            number_of_guesses = 5
         
         # If the level is medium, generate a random number between 1 and 50
         elif level == 'medium':
             request.session.pop('correct_number', None)
             request.session['correct_number'] = random.randint(1,50)
+            number_of_guesses = 10
 
         # If the level is hard, generate a random number between 1 and 100
         elif level == 'hard':
             request.session.pop('correct_number', None)
             request.session['correct_number'] = random.randint(1,100)
+            number_of_guesses = 20
 
     return render(request, 'home.html')
 
