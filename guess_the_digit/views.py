d45dc9117e8455ae837bd0d192d5331fd5d1838c
amar2468 <plakaloam@gmail.com>
2023-10-09 17:40:45 +0100

    Implemented the hint functionality by calling the function and returning whether the value is odd or even. This would only be possible if the user specified that they wanted hints

--- guess_the_digit/views.py
+++ guess_the_digit/views.py
@@ -8,6 +8,7 @@ import random
 def guess_the_digit_game(request):
     global number_of_guesses
     global level
+    global specific_hint
     if request.method == "POST":
 
         # Take the guess from the user
@@ -49,7 +50,8 @@ def guess_the_digit_game(request):
         # The result is stored in the context so it can be returned back to the html template
         context = {
             'result': result,
-            'latest_score': request.session.get('score')
+            'latest_score': request.session.get('score'),
+            'specific_hint' : specific_hint
         }
 
 
@@ -57,22 +59,43 @@ def guess_the_digit_game(request):
     
     level = request.GET.get('selected_level')
 
+    hints = request.GET.get('hints')
+
+    # Generate the hint (if number is odd or even) depending on the current condition
+    def create_hint(odd_or_even):
+        if hints == "yes":
+            if odd_or_even % 2 == 0:
+                return "The number is even"
+            else:
+                return "The number is odd"
+        else:
+            return ""
+
     # Check if the correct number has been specified in the current session
     if 'correct_number' not in request.session:
 
         # If the level is easy, generate a random number between 1 and 10
         if level == 'easy':
             request.session['correct_number'] = random.randint(1,10)
+
+            specific_hint = create_hint(request.session['correct_number'])
+
             number_of_guesses = 5
         
         # If the level is medium, generate a random number between 1 and 50
         elif level == 'medium':
             request.session['correct_number'] = random.randint(1,50)
+
+            specific_hint = create_hint(request.session['correct_number'])
+
             number_of_guesses = 10
 
         # If the level is hard, generate a random number between 1 and 100
         elif level == 'hard':
             request.session['correct_number'] = random.randint(1,100)
+
+            specific_hint = create_hint(request.session['correct_number'])
+
             number_of_guesses = 20
     # If the correct number has already been specified, it needs to be re-generated
     if 'correct_number' in request.session:
@@ -81,21 +104,34 @@ def guess_the_digit_game(request):
         if level == 'easy':
             request.session.pop('correct_number', None)
             request.session['correct_number'] = random.randint(1,10)
+
+            specific_hint = create_hint(request.session['correct_number'])
+
             number_of_guesses = 5
         
         # If the level is medium, generate a random number between 1 and 50
         elif level == 'medium':
             request.session.pop('correct_number', None)
             request.session['correct_number'] = random.randint(1,50)
+            
+            specific_hint = create_hint(request.session['correct_number'])
+
             number_of_guesses = 10
 
         # If the level is hard, generate a random number between 1 and 100
         elif level == 'hard':
             request.session.pop('correct_number', None)
             request.session['correct_number'] = random.randint(1,100)
-            number_of_guesses = 20
 
-    return render(request, 'home.html')
+            specific_hint = create_hint(request.session['correct_number'])
+
+            number_of_guesses = 20
+    
+    context = {
+        'specific_hint' : specific_hint
+    }
+
+    return render(request, 'home.html', context)
 
 # View that presents the configuration page for the guessing game
 def guess_the_digit_config(request):
