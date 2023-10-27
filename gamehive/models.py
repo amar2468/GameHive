4b6b12787386e608dd3af5db90b2aa7a900bf72b
amar2468 <plakaloam@gmail.com>
2023-10-27 18:17:38 +0100

    Created custom model for storing the testimonial records from users

--- gamehive/models.py
+++ gamehive/models.py
@@ -4,3 +4,9 @@ from django.contrib.auth.models import User
 class GameUserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     current_score = models.IntegerField(default=0)
+
+class TestimonialsModel(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    date_of_testimonial = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
