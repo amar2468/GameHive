2eac74e4778a83ca35655220d26b6ebc99f92c11
amar2468 <plakaloam@gmail.com>
2023-11-29 16:56:22 +0000

    Added more urls which will deal with the updating of personal details, updating of the password, and displaying the computer option when form is submitted (i.e. when user chooses their option)

--- rock_paper_scissors/urls.py
+++ rock_paper_scissors/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'rock_paper_scissors'
 
 urlpatterns = [
-    path('play/', views.play, name='play')
+    path('play/', views.play, name='play'),
+    path('rps_form_submitted/', views.rps_form_submitted, name='rps_form_submitted'),
+    path('update_personal_details/', views.update_personal_details, name='update_personal_details'),
+    path('change_password/', views.change_password, name='change_password')
 ]
