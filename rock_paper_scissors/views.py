fbec3fe6bb8c99b5fe5bdc026e2b4ad8e45baed2
amar2468 <plakaloam@gmail.com>
2023-11-30 15:24:15 +0000

    Added the outcome part of the game by comparing the user's choice against the computer's choice. More comments added also.

--- rock_paper_scissors/views.py
+++ rock_paper_scissors/views.py
@@ -9,16 +9,40 @@ import random
 def play(request):
     return render(request, 'rock_paper_scissors_play.html')
 
-# This view will allow the computer to choose a random option (rock,paper, or scissors) and inform the user of the choice
+# This view will allow the computer to choose a random option (rock,paper, or scissors) and inform the user of the choice.
+# Additionally, the user's choice is compared against the computer's choice and an outcome is returned to the user (win,lose,draw)
 # Note: This view is STILL BEING DEVELOPED.
 
 def rps_form_submitted(request):
     if request.method == "POST":
-        computer_choice_between_rps = ["Rock", "Paper", "Scissors"]
 
+        user_rps_choice = request.POST.get('carousel_value', '')
+
+        # Determine all the possible outcomes for the rock, paper, scissors game
+        rps_outcomes = {
+            'rock' : {'paper' : 'lose', 'scissors' : 'win'},
+            'paper' : {'rock' : 'win', 'scissors' : 'lose'},
+            'scissors' : {'rock' : 'lose', 'paper' : 'win'}
+        }
+
+        computer_choice_between_rps = ["rock", "paper", "scissors"]
+
+        # Pick a random item from the rock,paper,scissors list (presented as computer's choice)
         computer_rps_choice = random.choice(computer_choice_between_rps)
 
-        return JsonResponse({'computer_rps_choice' : computer_rps_choice})
+        # Using the rps_outcomes dictionary, the key (user's choice) will be found and the value (the computer's choice)
+        # will be the value in the dictionary. If it is a draw, a draw will be returned
+        rps_outcome = rps_outcomes[user_rps_choice].get(computer_rps_choice, 'draw')
+
+        # Adding the user's choice, computer's choice, and the outcome of the round back to the user so they are informed
+        response_info = {
+            'computer_rps_choice' : computer_rps_choice,
+            'user_rps_choice' : user_rps_choice,
+            'rps_outcome': rps_outcome
+        }
+
+        # Returning the dictionary in JSON format to jQuery, which will then be inserted into the HTML template
+        return JsonResponse(response_info)
     return render(request, 'rock_paper_scissors_play.html')
 
 # This view will update the personal details of the user (email, first name, and surname) - STILL BEING DEVELOPED
