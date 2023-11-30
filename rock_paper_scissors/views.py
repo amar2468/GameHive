d61730ccddf84ea8a1a9203e41e83f2dcc320e4d
amar2468 <plakaloam@gmail.com>
2023-11-30 15:51:00 +0000

    Added "attempts" logic which only allows a user 3 attempts in the rock,paper,scissors (rps) game.

--- rock_paper_scissors/views.py
+++ rock_paper_scissors/views.py
@@ -16,6 +16,8 @@ def play(request):
 def rps_form_submitted(request):
     if request.method == "POST":
 
+        attempts = request.session.get('attempts', 3)
+
         user_rps_choice = request.POST.get('carousel_value', '')
 
         # Determine all the possible outcomes for the rock, paper, scissors game
@@ -30,16 +32,30 @@ def rps_form_submitted(request):
         # Pick a random item from the rock,paper,scissors list (presented as computer's choice)
         computer_rps_choice = random.choice(computer_choice_between_rps)
 
-        # Using the rps_outcomes dictionary, the key (user's choice) will be found and the value (the computer's choice)
-        # will be the value in the dictionary. If it is a draw, a draw will be returned
-        rps_outcome = rps_outcomes[user_rps_choice].get(computer_rps_choice, 'draw')
+        if attempts > 0:
+            attempts -= 1
+            request.session['attempts'] = attempts
 
-        # Adding the user's choice, computer's choice, and the outcome of the round back to the user so they are informed
-        response_info = {
-            'computer_rps_choice' : computer_rps_choice,
-            'user_rps_choice' : user_rps_choice,
-            'rps_outcome': rps_outcome
-        }
+            # Using the rps_outcomes dictionary, the key (user's choice) will be found and the value (the computer's choice)
+            # will be the value in the dictionary. If it is a draw, a draw will be returned
+            rps_outcome = rps_outcomes[user_rps_choice].get(computer_rps_choice, 'draw')
+
+            # Adding the user's choice, computer's choice, and the outcome of the round back to the user so they are informed
+            response_info = {
+                'computer_rps_choice' : computer_rps_choice,
+                'user_rps_choice' : user_rps_choice,
+                'rps_outcome': rps_outcome,
+                'attempts' : attempts
+            }
+        else:
+            del request.session['attempts']
+
+            # Adding the user's choice, computer's choice, and informing user that the round is over
+            response_info = {
+                'computer_rps_choice' : computer_rps_choice,
+                'user_rps_choice' : user_rps_choice,
+                'rps_outcome': 'Game Over!'
+            }
 
         # Returning the dictionary in JSON format to jQuery, which will then be inserted into the HTML template
         return JsonResponse(response_info)
