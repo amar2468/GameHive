cd5c382d8402ae52277a0f0a3c14510cf08185c1
amar2468 <plakaloam@gmail.com>
2023-12-01 13:16:16 +0000

    Removed two urls from here and placed them in the correct urls file (GameHive urls file)

--- rock_paper_scissors/urls.py
+++ rock_paper_scissors/urls.py
@@ -5,7 +5,5 @@ app_name = 'rock_paper_scissors'
 
 urlpatterns = [
     path('play/', views.play, name='play'),
-    path('rps_form_submitted/', views.rps_form_submitted, name='rps_form_submitted'),
-    path('update_personal_details/', views.update_personal_details, name='update_personal_details'),
-    path('change_password/', views.change_password, name='change_password')
+    path('rps_form_submitted/', views.rps_form_submitted, name='rps_form_submitted')
 ]
