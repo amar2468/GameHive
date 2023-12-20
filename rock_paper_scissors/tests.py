4711f44efc09f7c23dc0675f61d629b7d15f7204
amar2468 <plakaloam@gmail.com>
2023-12-20 14:49:06 +0000

    Created a simple unit test for the rock,paper,scissors game that will test a valid input (more will come soon)

---
+++ rock_paper_scissors/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase, Client
+from .forms import RockPaperScissorsInputForm
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+# Unit Test for Rock, Paper, Scissors game
+
+class RockPaperScissorsTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='Password34*')
+        self.client = Client()
+    
+    def testing_valid_input_rps(self):
+
+        self.client.login(username='testuser', password='Password34*')
+
+        data = {
+            'carousel_value' : 'rock'
+        }
+
+        form = RockPaperScissorsInputForm(data=data)
+
+        self.assertTrue(form.is_valid())
