eea961a274272da48925c194e310b299b7fa7a29
amar2468 <plakaloam@gmail.com>
2024-01-26 13:14:34 +0000

    Created separate urls for single player & multiplayer game modes for rock, paper, scissors game

--- rock_paper_scissors/urls.py
+++ rock_paper_scissors/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'rock_paper_scissors'
 
 urlpatterns = [
-    path('play/', views.play, name='play'),
+    path('single_player_rps/', views.single_player_rps, name='single_player_rps'),
+    path('multiplayer_rps/', views.multiplayer_rps, name='multiplayer_rps'),
     path('rps_form_submitted/', views.rps_form_submitted, name='rps_form_submitted')
 ]
