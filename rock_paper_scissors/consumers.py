09a15de3973622ce8e51e2f89b1afab0c7bb5316
amar2468 <plakaloam@gmail.com>
2024-03-07 15:27:51 +0000

    Saved the points that the user won in the multiplayer mode in the relevant model, so that the total score is updated

--- rock_paper_scissors/consumers.py
+++ rock_paper_scissors/consumers.py
@@ -1,5 +1,8 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+from django.contrib.auth.models import User
+from gamehive.models import GameUserProfile
+from asgiref.sync import sync_to_async
 
 class RockPaperScissorsConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -15,6 +18,12 @@ class RockPaperScissorsConsumer(AsyncWebsocketConsumer):
 
         global user2_name
         user2_name = ""
+
+        global user1_wins
+        user1_wins = 0
+
+        global user2_wins
+        user2_wins = 0
 
         await self.channel_layer.group_add(self.rps_room_name, self.channel_name)
 
@@ -53,10 +62,14 @@ class RockPaperScissorsConsumer(AsyncWebsocketConsumer):
         global user1_name
         global user2_name
 
+        global user1_wins
+        global user2_wins
+
         self.room_state.setdefault('rps_options', {})
         self.room_state['rps_options'][username] = {
             "user_option" : user_option,
-            "rps_outcome": ""
+            "outcome_of_the_round": "",
+            "total_wins" : 0
         }
 
         self.room_state.setdefault('attempts', 3)
@@ -76,7 +89,6 @@ class RockPaperScissorsConsumer(AsyncWebsocketConsumer):
             if user1_choice != "" and user2_choice != "":
 
                 if self.attempts > 0:
-                    self.attempts -= 1
 
                     rps_outcomes = {
                         'rock' : {'paper' : 'lose', 'scissors' : 'win'},
@@ -87,24 +99,64 @@ class RockPaperScissorsConsumer(AsyncWebsocketConsumer):
                     rps_outcome_user_1 = rps_outcomes[user1_choice].get(user2_choice, 'draw')
                     rps_outcome_user_2 = rps_outcomes[user2_choice].get(user1_choice, 'draw')
 
+                    if rps_outcome_user_1 == "win":
+                        user1_wins += 1
+                        self.attempts -= 1 # only reduce the number of attempts left if either user won
+                    
+                    elif rps_outcome_user_2 == "win":
+                        user2_wins += 1
+                        self.attempts -= 1 # only reduce the number of attempts left if either user won
+                    
+                    if self.attempts == 0:
+                        if user1_wins > user2_wins:
+                            rps_outcome_user_1 = "Game Over! You won this round! You have received 10 points!"
+                            rps_outcome_user_2 = "Game Over! You failed to win this game! Good luck next time!"
+                            
+                            try:
+                                user_instance = await sync_to_async(User.objects.get)(username=user1_name)
+                                game_user_profile = await sync_to_async(GameUserProfile.objects.get)(user=user_instance)
+                                game_user_profile.current_score += 10
+                                await sync_to_async(game_user_profile.save)()
+                            except GameUserProfile.DoesNotExist:
+                                user_instance = await sync_to_async(User.objects.get)(username=user1_name)
+                                game_user_profile = await sync_to_async(GameUserProfile.objects.get)(user=user_instance)
+                                game_user_profile.current_score += 10
+                                await sync_to_async(game_user_profile.save)()
+                        elif user2_wins > user1_wins:
+                            rps_outcome_user_1 = "Game Over! You failed to win this game! Good luck next time!"
+                            rps_outcome_user_2 = "Game Over! You won this round! You have received 10 points!"
+
+                            try:
+                                user_instance_2 = await sync_to_async(User.objects.get)(username=user2_name)
+                                game_user_profile = await sync_to_async(GameUserProfile.objects.get)(user=user_instance_2)
+                                game_user_profile.current_score += 10
+                                await sync_to_async(game_user_profile.save)()
+                            except GameUserProfile.DoesNotExist:
+                                user_instance_2 = await sync_to_async(User.objects.get)(username=user2_name)
+                                game_user_profile = await sync_to_async(GameUserProfile.objects.get)(user=user_instance_2)
+                                game_user_profile.current_score += 10
+                                await sync_to_async(game_user_profile.save)()
+
                     self.room_state['rps_options'][user1_name] = {
                         "user_option" : user1_choice,
-                        "rps_outcome": rps_outcome_user_1
+                        "outcome_of_the_round": rps_outcome_user_1,
+                        "total_wins" : user1_wins
                     }
 
                     self.room_state['rps_options'][user2_name] = {
                         "user_option" : user2_choice,
-                        "rps_outcome": rps_outcome_user_2
+                        "outcome_of_the_round": rps_outcome_user_2,
+                        "total_wins" : user2_wins
                     }
 
-                    #self.room_state['rps_options'] = {}
+                    await self.send(
+                        text_data=json.dumps(
+                            {
+                                "user_list" : [user1_name, user2_name],
+                                "rps_options": self.room_state['rps_options'],
+                                "attempts" : self.attempts
+                            }
+                        )
+                    )
 
-        await self.send(
-            text_data=json.dumps(
-                {
-                    "user_list" : [user1_name, user2_name],
-                    "rps_options": self.room_state['rps_options'],
-                    "attempts" : self.attempts
-                }
-            )
-        )
+                    self.room_state['rps_options'] = {}
