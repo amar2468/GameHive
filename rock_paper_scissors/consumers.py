00075200836c85aaa7b8db11067b56cb9b1bdbea
amar2468 <plakaloam@gmail.com>
2024-12-23 11:11:51 +0000

    Removed global definitiion of variables & made minor changes

--- rock_paper_scissors/consumers.py
+++ rock_paper_scissors/consumers.py
@@ -13,18 +13,6 @@ class RockPaperScissorsConsumer(AsyncWebsocketConsumer):
 
         self.attempts = 3 # initialise the number of attempts for this round
 
-        global user1_name
-        user1_name = ""
-
-        global user2_name
-        user2_name = ""
-
-        global user1_wins
-        user1_wins = 0
-
-        global user2_wins
-        user2_wins = 0
-
         await self.channel_layer.group_add(self.rps_room_name, self.channel_name)
 
         await self.accept()
@@ -56,35 +44,27 @@ class RockPaperScissorsConsumer(AsyncWebsocketConsumer):
             print(f"Error decoding JSON: {e}")
     
     async def rps_move(self, event):
-        user = str(self.scope["user"])
         user_option = event["user_option"]
         username = event["username"]
-        global user1_name
-        global user2_name
-
-        global user1_wins
-        global user2_wins
 
         self.room_state.setdefault('rps_options', {})
-        self.room_state['rps_options'][username] = {
-            "user_option" : user_option,
-            "outcome_of_the_round": "",
-            "total_wins" : 0
-        }
+
+        self.room_state['rps_options'].setdefault(username, {
+            "user_option": user_option,
+            "outcome_of_attempt": "",
+            "outcome_of_round" : "",
+            "total_wins": 0
+        })
+        self.room_state['rps_options'][username]["user_option"] = user_option
 
         self.room_state.setdefault('attempts', 3)
 
         if len(self.room_state['rps_options']) == 2:
+            
+            user1_name, user2_name = list(self.room_state['rps_options'].keys())
 
-
-            for username in self.room_state['rps_options'].keys():
-                if username != user:
-                    user2_choice = self.room_state['rps_options'][username]['user_option']
-                    user2_name = username
-                else:
-                    user1_choice = self.room_state['rps_options'][user]['user_option']
-                    user1_name = user
-                    
+            user1_choice = self.room_state['rps_options'][user1_name]['user_option']
+            user2_choice = self.room_state['rps_options'][user2_name]['user_option']
 
             if user1_choice != "" and user2_choice != "":
 
@@ -100,17 +80,16 @@ class RockPaperScissorsConsumer(AsyncWebsocketConsumer):
                     rps_outcome_user_2 = rps_outcomes[user2_choice].get(user1_choice, 'draw')
 
                     if rps_outcome_user_1 == "win":
-                        user1_wins += 1
+                        self.room_state['rps_options'][user1_name]['total_wins'] += 1
                         self.attempts -= 1 # only reduce the number of attempts left if either user won
-                    
                     elif rps_outcome_user_2 == "win":
-                        user2_wins += 1
-                        self.attempts -= 1 # only reduce the number of attempts left if either user won
+                        self.room_state['rps_options'][user2_name]['total_wins'] += 1
+                        self.attempts -= 1
                     
                     if self.attempts == 0:
-                        if user1_wins > user2_wins:
-                            rps_outcome_user_1 = "Game Over! You won this round! You have received 10 points!"
-                            rps_outcome_user_2 = "Game Over! You failed to win this game! Good luck next time!"
+                        if self.room_state['rps_options'][user1_name]['total_wins'] > self.room_state['rps_options'][user2_name]['total_wins']:
+                            self.room_state['rps_options'][user1_name]["outcome_of_round"] = "Game Over! You won this round! You have received 10 points!"
+                            self.room_state['rps_options'][user2_name]["outcome_of_round"] = "Game Over! You failed to win this game! Good luck next time!"
                             
                             try:
                                 user_instance = await sync_to_async(User.objects.get)(username=user1_name)
@@ -122,9 +101,10 @@ class RockPaperScissorsConsumer(AsyncWebsocketConsumer):
                                 game_user_profile = await sync_to_async(GameUserProfile.objects.get)(user=user_instance)
                                 game_user_profile.current_score += 10
                                 await sync_to_async(game_user_profile.save)()
-                        elif user2_wins > user1_wins:
-                            rps_outcome_user_1 = "Game Over! You failed to win this game! Good luck next time!"
-                            rps_outcome_user_2 = "Game Over! You won this round! You have received 10 points!"
+                            
+                        elif self.room_state['rps_options'][user2_name]['total_wins'] > self.room_state['rps_options'][user1_name]['total_wins']:
+                            self.room_state['rps_options'][user1_name]["outcome_of_round"] = "Game Over! You failed to win this game! Good luck next time!"
+                            self.room_state['rps_options'][user2_name]["outcome_of_round"] = "Game Over! You won this round! You have received 10 points!"
 
                             try:
                                 user_instance_2 = await sync_to_async(User.objects.get)(username=user2_name)
@@ -139,15 +119,19 @@ class RockPaperScissorsConsumer(AsyncWebsocketConsumer):
 
                     self.room_state['rps_options'][user1_name] = {
                         "user_option" : user1_choice,
-                        "outcome_of_the_round": rps_outcome_user_1,
-                        "total_wins" : user1_wins
+                        "outcome_of_attempt": rps_outcome_user_1,
+                        "outcome_of_round" : self.room_state['rps_options'][user1_name]["outcome_of_round"],
+                        "total_wins" : self.room_state['rps_options'][user1_name]['total_wins']
                     }
 
                     self.room_state['rps_options'][user2_name] = {
                         "user_option" : user2_choice,
-                        "outcome_of_the_round": rps_outcome_user_2,
-                        "total_wins" : user2_wins
+                        "outcome_of_attempt": rps_outcome_user_2,
+                        "outcome_of_round" : self.room_state['rps_options'][user2_name]["outcome_of_round"],
+                        "total_wins" : self.room_state['rps_options'][user2_name]['total_wins']
                     }
+
+                    print(self.room_state['rps_options'][user1_name])
 
                     await self.send(
                         text_data=json.dumps(
@@ -159,4 +143,8 @@ class RockPaperScissorsConsumer(AsyncWebsocketConsumer):
                         )
                     )
 
-                    self.room_state['rps_options'] = {}
+                    self.room_state['rps_options'][user1_name]["user_option"] = ""
+                    self.room_state['rps_options'][user2_name]["user_option"] = ""
+                    
+                    self.room_state['rps_options'][user1_name]["outcome_of_attempt"] = ""
+                    self.room_state['rps_options'][user2_name]["outcome_of_attempt"] = ""
