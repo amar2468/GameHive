1043e57f4a1d8fd1247a94323ccb32c9efc91f05
amar2468 <plakaloam@gmail.com>
2025-01-19 17:49:20 +0000

    Fixed a bug where the rps game has 4 rounds, instead of 3.

--- rock_paper_scissors/views.py
+++ rock_paper_scissors/views.py
@@ -64,66 +64,87 @@ def rps_form_submitted(request):
             # Pick a random item from the rock,paper,scissors list (presented as computer's choice)
             computer_rps_choice = random.choice(computer_choice_between_rps)
 
-            if attempts > 0:
-                attempts -= 1
-                request.session['attempts'] = attempts
+            if attempts >= 1:
+                if attempts == 1:
+                    rps_end_of_game = ""
+                    attempts -= 1
+                    request.session['attempts'] = attempts
 
-                # Using the rps_outcomes dictionary, the key (user's choice) will be found and the value (the computer's choice)
-                # will be the value in the dictionary. If it is a draw, a draw will be returned
-                rps_outcome = rps_outcomes[user_rps_choice].get(computer_rps_choice, 'draw')
+                    # Using the rps_outcomes dictionary, the key (user's choice) will be found and the value (the computer's choice)
+                    # will be the value in the dictionary. If it is a draw, a draw will be returned
+                    rps_round_outcome = rps_outcomes[user_rps_choice].get(computer_rps_choice, 'draw')
 
-                if rps_outcome == "win":
-                    total_wins += 1
-                    request.session['total_wins'] = total_wins
+                    if rps_round_outcome == "win":
+                        total_wins += 1
+                        request.session['total_wins'] = total_wins
+                    
+                    # Increasing the round number
+                    rounds += 1
+                    request.session['rounds'] = rounds
                 
-                # Increasing the round number
-                rounds += 1
-                request.session['rounds'] = rounds
+                    # Remove the "attempts" session variable as the game has finished so it should be reset back to 3 attempts for the new one
 
-                # Adding the user's choice, computer's choice, and the outcome of the round back to the user so they are informed
-                response_info = {
-                    'computer_rps_choice' : computer_rps_choice,
-                    'user_rps_choice' : user_rps_choice,
-                    'rps_outcome': rps_outcome,
-                    'attempts' : attempts,
-                    'round_number' : rounds
-                }
-            else:
-                # Remove the "attempts" session variable as the game has finished so it should be reset back to 3 attempts for the new one
+                    del request.session['attempts']
 
-                del request.session['attempts']
+                    # Remove the "rounds" session variable as the game has finished and it should be reset for the new game
+                    del request.session['rounds']
 
-                # Remove the "rounds" session variable as the game has finished and it should be reset for the new game
-                del request.session['rounds']
+                    # In every round, the user has three attempts. If they get 2 or more attempts correct, they won that round. Hence 10
+                    # points would be added to their total score
 
-                # In every round, the user has three attempts. If they get 2 or more attempts correct, they won that round. Hence 10
-                # points would be added to their total score
+                    if 'total_wins' in request.session:
+                        if request.session['total_wins'] >= 2:
+                                rps_end_of_game = "Game Over! You won this round! You have received 10 points!"
+                                try:
+                                    game_user_profile = GameUserProfile.objects.get(user=request.user)
+                                except GameUserProfile.DoesNotExist:
+                                    game_user_profile = GameUserProfile(user=request.user)
+                                game_user_profile.current_score += 10
+                                game_user_profile.save()
+                        else:
+                            rps_end_of_game = "Game Over! You failed to win this round! Good luck next time!"
 
-                if 'total_wins' in request.session:
-                    if request.session['total_wins'] >= 2:
-                            rps_outcome = "Game Over! You won this round! You have received 10 points!"
-                            try:
-                                game_user_profile = GameUserProfile.objects.get(user=request.user)
-                            except GameUserProfile.DoesNotExist:
-                                game_user_profile = GameUserProfile(user=request.user)
-                            game_user_profile.current_score += 10
-                            game_user_profile.save()
+                        # Remove the "total_score" session variable as the score has been saved in the GameUserProfile custom model
+                    
+                        del request.session['total_wins']
+                    
                     else:
-                        rps_outcome = "Game Over! You failed to win this round! Good luck next time!"
+                        rps_end_of_game = "Game Over! You failed to win this round! Good luck next time!"
 
-                    # Remove the "total_score" session variable as the score has been saved in the GameUserProfile custom model
-                
-                    del request.session['total_wins']
+                    # Adding the user's choice, computer's choice, and informing user that the round is over
+                    response_info = {
+                        'round_number' : rounds,
+                        'computer_rps_choice' : computer_rps_choice,
+                        'user_rps_choice' : user_rps_choice,
+                        'rps_round_outcome': rps_round_outcome,
+                        'rps_end_of_game': rps_end_of_game
+                    }
                 
                 else:
-                    rps_outcome = "Game Over! You failed to win this round! Good luck next time!"
+                    attempts -= 1
+                    request.session['attempts'] = attempts
 
-                # Adding the user's choice, computer's choice, and informing user that the round is over
-                response_info = {
-                    'computer_rps_choice' : computer_rps_choice,
-                    'user_rps_choice' : user_rps_choice,
-                    'rps_outcome': rps_outcome
-                }
+                    # Using the rps_outcomes dictionary, the key (user's choice) will be found and the value (the computer's choice)
+                    # will be the value in the dictionary. If it is a draw, a draw will be returned
+                    rps_round_outcome = rps_outcomes[user_rps_choice].get(computer_rps_choice, 'draw')
+
+                    if rps_round_outcome == "win":
+                        total_wins += 1
+                        request.session['total_wins'] = total_wins
+                    
+                    # Increasing the round number
+                    rounds += 1
+                    request.session['rounds'] = rounds
+
+                    # Adding the user's choice, computer's choice, and the outcome of the round back to the user so they are informed
+                    response_info = {
+                        'computer_rps_choice' : computer_rps_choice,
+                        'user_rps_choice' : user_rps_choice,
+                        'rps_round_outcome': rps_round_outcome,
+                        'attempts' : attempts,
+                        'round_number' : rounds,
+                        'rps_end_of_game' : ""
+                    }
 
             # Returning the dictionary in JSON format to jQuery, which will then be inserted into the HTML template
             return JsonResponse(response_info)
