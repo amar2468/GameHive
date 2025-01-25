a32b62d50b12cf0a6fb181da1f8631c3519aaa82
amar2468 <plakaloam@gmail.com>
2025-01-25 17:05:46 +0000

    Removed the config URL as it is no longer needed for this project

--- rock_paper_scissors/urls.py
+++ rock_paper_scissors/urls.py
@@ -5,7 +5,6 @@ app_name = 'rock_paper_scissors'
 
 urlpatterns = [
     path('single_player_rps/', views.single_player_rps, name='single_player_rps'),
-    path('multiplayer_rps_config/', views.multiplayer_rps_config, name='multiplayer_rps_config'),
     path('multiplayer_rps_start_game/', views.multiplayer_rps_start_game, name='multiplayer_rps_start_game'),
     path('rps_form_submitted/', views.rps_form_submitted, name='rps_form_submitted')
 ]
