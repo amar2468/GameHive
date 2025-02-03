c4b43a8933de4ebaf3cbb5bf450fef6fcf9f6ec4
amar2468 <plakaloam@gmail.com>
2025-02-03 13:20:49 +0000

    Added multiplayer RPS URL here, as the URL belongs here

--- rock_paper_scissors/urls.py
+++ rock_paper_scissors/urls.py
@@ -6,5 +6,6 @@ app_name = 'rock_paper_scissors'
 urlpatterns = [
     path('single_player_rps/', views.single_player_rps, name='single_player_rps'),
     path('multiplayer_rps_start_game/', views.multiplayer_rps_start_game, name='multiplayer_rps_start_game'),
+    path('multiplayer_rps/<room_id>/', views.multiplayer_rps_start_game, name='multiplayer_rps'),
     path('rps_form_submitted/', views.rps_form_submitted, name='rps_form_submitted')
 ]
