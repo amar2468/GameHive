605d3afb0045036953ca66ee4e186591a607b8a7
amar2468 <plakaloam@gmail.com>
2025-02-14 21:24:23 +0000

    Added app name

--- guess_the_digit/urls.py
+++ guess_the_digit/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'guess_the_digit'
+
 urlpatterns = [
     path('play/', views.guess_the_digit_game, name='play'),
     path('config/', views.guess_the_digit_config, name='config')
