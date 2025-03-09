6efa0ff7b5364080287b2777f1cdf3f449087ba8
amar2468 <plakaloam@gmail.com>
2025-03-09 11:21:01 +0000

    Added CustomUser model, instead of default User model, as there are options to add customised attributes to it.

--- gamehive/models.py
+++ gamehive/models.py
@@ -1,17 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+class CustomUser(AbstractUser):
+    account_type = models.CharField(max_length=25)
 
 class GameUserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     current_score = models.IntegerField(default=0)
 
 class PersonalDetails(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=25)
     surname = models.CharField(max_length=25)
 
 class TestimonialsModel(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_testimonial = models.DateTimeField(auto_now_add=True)
     message = models.TextField()
     star_rating = models.IntegerField()
