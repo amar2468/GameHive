74ebe1fcc222dbef5b1848a6737ff0cc5ae6a223
amar2468 <plakaloam@gmail.com>
2025-03-26 12:08:04 +0000

    Removed PersonalDetails model as we are putting the fields from this model into the CustomUser model instead.

--- gamehive/models.py
+++ gamehive/models.py
@@ -9,11 +9,6 @@ class GameUserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     current_score = models.IntegerField(default=0)
 
-class PersonalDetails(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=25)
-    surname = models.CharField(max_length=25)
-
 class TestimonialsModel(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_testimonial = models.DateTimeField(auto_now_add=True)
