534443f04338a1a736819c5a8beac1826632213a
amar2468 <plakaloam@gmail.com>
2025-04-07 16:53:27 +0100

    Added URL reference in the AJAX call to reference data.url attribute in HTML + added the function to deal with deleting users/testimonials. An AJAX POST request is made, and the list of users/testimonials is passed to the backend.

--- static/js/manage_users.js
+++ static/js/manage_users.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
     $('#refresh_users_btn').click(function() {
         $.ajax({
             type: 'GET',
-            url: "{% url 'manage_users' %}",
+            url: $('.admin_options_delete').data("url"),
 
             // If the page refresh was successful, we will perform the below.
             success: function(response) {
@@ -43,6 +43,79 @@ $(document).ready(function() {
             }
 
         });
+    });
+
+    // If the admin clicks on the button to delete a user/users, a POST request will be performed, sending the relevant
+    // info to the backend for processing.
+    $(document).on("click", ".admin_options_delete", function() {
+        // Creating an empty list that will store the users that need to be deleted (this would be all the users that the admin
+        // selected using the checkbox)
+        let users_to_delete_list = [];
+
+        // Iterating through all the rows that were "checked", and then finding the username of the user to delete
+        $('.select-row:checked').each(function() {
+            // Find the row where this checkbox resides in
+            let checked_row = $(this).closest("tr");
+
+            // Within the row where the checkbox resides, find an element that stores the username of that user
+            let username_to_delete = checked_row.find("#username_link_manage_users");
+
+            // Add the username to delete into the array
+            users_to_delete_list.push(username_to_delete[0].textContent);
+        });
+
+        // When the admin chooses the user(s) to delete and clicks the button to delete the users, a POST request will
+        // be made and the list of user(s) to delete will be sent to the backend for processing.
+        $.ajax({
+            type: 'POST',
+            url: $('.admin_options_delete').data("url"),
+            data: JSON.stringify({
+                users_to_delete_list : users_to_delete_list
+            }),
+            // As Django protects all POST requests with CSRF protection, we will need to use the csrf token when making the request
+            headers: {
+                "X-CSRFToken": $('#csrf_token').val()
+            },
+
+            // If the POST request was successful (no errors encountered from the backend), we will verify whether the user(s) record
+            // has been removed.
+            success: function(response) {
+                // If the deletion of the user(s) was successful, we will just populate the paragraph element with the success
+                // message and display the div that holds the paragraph element.
+                if (response.success === true) {
+                    $('#paragraph_outcome_delete_user_success').text(response.status);
+                    $('#div_outcome_delete_user_success').show();
+                }
+                
+                // If the deletion of the user(s) was unsuccessful, we will just populate the paragraph element with the failure
+                // message and display the div that holds the paragraph element.
+                else {
+                    $('#paragraph_outcome_delete_user_failure').text(response.status);
+                    $('#div_outcome_delete_testimonial_success').show();
+                }
+
+                // After 1 second, we want to refresh (not reload) the page, removing the success/failure messages and showing
+                // the up-to-date user table.
+                setTimeout(function() {
+                    // We are clearing the success message and hiding the associated div, as we have already informed the admin
+                    $('#paragraph_outcome_delete_user_success').text("");
+                    $('#div_outcome_delete_user_success').hide();
+
+                    // We are clearing the failure message and hiding the associated div, as we have already informed the admin
+                    $('#paragraph_outcome_delete_user_failure').text("");
+                    $('#div_outcome_delete_testimonial_success').hide();
+
+                    // Refresh (not reload) the page by clicking on the refresh button.
+                    $('#refresh_users_btn').click();
+                }, 1000);
+            },
+
+            // If an error occurred, the error will be logged to the console.
+            error: function(error) {
+                console.log(error);
+            }
+        });
+
     });
 
     // If the user clicks on the "select all" button, it will either select all or unselect all options.
--- static/js/testimonials_mgmt.js
+++ static/js/testimonials_mgmt.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
     $('#refresh_testimonials_btn').click(function() {
         $.ajax({
             type: 'GET',
-            url: "{% url 'testimonials_mgmt' %}",
+            url: $('.admin_options_delete').data("url"),
 
             // If the page refresh was successful, we will perform the below.
             success: function(response) {
@@ -43,6 +43,80 @@ $(document).ready(function() {
                 console.log(error);
             }
         })
+    });
+
+    // If the admin clicks on the button to delete a testimonial/testimonials, a POST request will be performed, sending the relevant
+    // info to the backend for processing.
+    $(document).on("click", ".admin_options_delete", function() {
+        // Creating an empty list that will store the testimonials that need to be deleted (this would be all the
+        // testimonials that the admin selected using the checkbox)
+        let testimonials_to_delete = [];
+
+        // Iterating through all the rows that were "checked", and then finding the testimonial to delete
+        $('.select-row:checked').each(function() {
+            // Find the row where this checkbox resides in
+            let checked_row = $(this).closest("tr");
+
+            // Within the row where the checkbox resides, find an element that stores the author of the testimonial, which will
+            // be the username in this case.
+            let testimonial_to_delete = checked_row.find("#author_of_testimonial");
+
+            // Add the testimonial that needs to be deleted into the array.
+            testimonials_to_delete.push(testimonial_to_delete[0].textContent);
+        });
+
+        // When the admin chooses the testimonial(s) to delete and clicks the button to delete the testimonial(s), a POST request will
+        // be made and the list of testimonial(s) to delete will be sent to the backend for processing.
+        $.ajax({
+            type: 'POST',
+            url: $('.admin_options_delete').data("url"),
+            data: JSON.stringify({
+                'testimonials_to_delete': testimonials_to_delete
+            }),
+            // As Django protects all POST requests with CSRF protection, we will need to use the csrf token when making the request
+            headers: {
+                "X-CSRFToken": $('#csrf_token').val()
+            },
+
+            // If the POST request was successful (no errors encountered from the backend), we will verify whether the testimonial(s) 
+            // record(s) has been removed.
+            success: function(response) {
+                // If the deletion of the testimonial(s) was successful, we will just populate the paragraph element with the success
+                // message and display the div that holds the paragraph element.
+                if (response.success === true) {
+                    $('#paragraph_outcome_delete_testimonial_success').text(response.status);
+                    $('#div_outcome_delete_testimonial_success').show();
+                }
+
+                // If the deletion of the testimonial(s) was unsuccessful, we will just populate the paragraph element with the failure
+                // message and display the div that holds the paragraph element.
+                else {
+                    $('#paragraph_outcome_delete_testimonial_failure').text(response.status);
+                    $('#div_outcome_delete_testimonial_failure').show();
+                }
+
+                // After 1 second, we want to refresh (not reload) the page, removing the success/failure messages and showing
+                // the up-to-date testimonial table.
+                setTimeout(function() {
+                    // We are clearing the success message and hiding the associated div, as we have already informed the admin
+                    $('#paragraph_outcome_delete_testimonial_success').text("");
+                    $('#div_outcome_delete_testimonial_success').hide();
+
+                    // We are clearing the failure message and hiding the associated div, as we have already informed the admin
+                    $('#paragraph_outcome_delete_testimonial_failure').text("");
+                    $('#div_outcome_delete_testimonial_failure').hide();
+
+                    // Refresh (not reload) the page by clicking on the refresh button.
+                    $('#refresh_testimonials_btn').click();
+                }, 1000);
+            },
+
+            // If an error occurred, the error will be logged to the console.
+            error: function(error) {
+                console.log(error);
+            }
+        });
+
     });
 
     // If the admin clicks on the "select all" button, it will either select all or unselect all options.
