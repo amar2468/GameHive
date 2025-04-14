4e700c90f377bc266e3ab106d18f312fdcb13f5c
amar2468 <plakaloam@gmail.com>
2025-04-14 12:57:12 +0100

    Moved the "ProtocolTypeRouter" code block to this file, as this is where it should be.

--- gamehive/asgi.py
+++ gamehive/asgi.py
@@ -1,7 +1,19 @@
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import gamehive.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamehive.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter( 
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+               gamehive.routing.websocket_urlpatterns
+            )
+        ),
+    }
+)
