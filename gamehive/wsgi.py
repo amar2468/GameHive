28b982700244c899e55b03715d8f6306b38c8bd6
amar2468 <plakaloam@gmail.com>
2025-04-14 21:38:20 +0100

    Changed the order of "DJANGO_SETTINGS_MODULE" and "import os"

--- gamehive/wsgi.py
+++ gamehive/wsgi.py
@@ -1,6 +1,6 @@
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamehive.settings")
-
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamehive.settings")
 
 from django.core.wsgi import get_wsgi_application
 
