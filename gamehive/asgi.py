816fc8366970ea62f25f6d82c2242458e3a36bf2
amar2468 <plakaloam@gmail.com>
2025-04-14 21:33:21 +0100

    Added the DJANGO_SETTINGS_MODULE import on the top of asgi.py and wsgi.py

--- gamehive/asgi.py
+++ gamehive/asgi.py
@@ -1,11 +1,10 @@
-import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamehive.settings")
 
+import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import gamehive.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamehive.settings")
 
 application = ProtocolTypeRouter( 
     {
--- gamehive/wsgi.py
+++ gamehive/wsgi.py
@@ -1,7 +1,7 @@
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamehive.settings")
+
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamehive.settings")
 
 application = get_wsgi_application()
