7a80736d9763b023b7ec3549e2f9849a4a3fd799
amar2468 <plakaloam@gmail.com>
2025-04-15 22:02:14 +0100

    Moved "get_asgi_application()" above "import gamehive.routing", in order to avoid error about the apps not being fully loaded.

--- gamehive/asgi.py
+++ gamehive/asgi.py
@@ -2,15 +2,17 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamehive.settings")
 
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+
+asgi_application = get_asgi_application()
+
 import gamehive.routing
 
 application = ProtocolTypeRouter( 
     {
-        "http": get_asgi_application(),
+        "http": asgi_application,
         "websocket": AuthMiddlewareStack(
             URLRouter(
                gamehive.routing.websocket_urlpatterns
