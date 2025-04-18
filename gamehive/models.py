414f7ff4dbfbcaf32ef26d7c29d8e6f40dbb1ac3
amar2468 <plakaloam@gmail.com>
2025-04-18 21:15:24 +0100

    Added the CustomerSupportModel, which will store the users request.

--- gamehive/models.py
+++ gamehive/models.py
@@ -14,3 +14,17 @@ class TestimonialsModel(models.Model):
     date_of_testimonial = models.DateTimeField(auto_now_add=True)
     message = models.TextField()
     star_rating = models.IntegerField()
+
+class CustomerSupportModel(models.Model):
+    requester_username = models.CharField(max_length=150)
+    requester_email = models.EmailField(blank=True)
+    ticket_id = models.BigAutoField(primary_key=True)
+    ticket_request_type = models.CharField(max_length=150)
+    ticket_title = models.CharField(max_length=150)
+    ticket_description = models.TextField()
+    ticket_attachments = models.FileField(upload_to="ticket_attachments/", blank=True, null=True)
+    ticket_status = models.CharField(max_length=150)
+    ticket_assigned_to = models.CharField(max_length=150)
+    ticket_created_at = models.DateTimeField(auto_now_add=True)
+    ticket_updated_at = models.DateTimeField(auto_now=True)
+    ticket_comments = models.TextField(default='[]', blank=True)
