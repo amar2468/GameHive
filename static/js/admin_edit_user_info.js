e4aee8f83186370170b2a23b7970acfa08ab1274
amar2468 <plakaloam@gmail.com>
2025-04-30 18:21:49 +0100

    Created a JS file for the page where the admin can view/update the user profile + added code to highlight the "Admin Dashboard" nav-link option when the user is on the page.

---
+++ static/js/admin_edit_user_info.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    // When on the page where the admin can view/edit the user profile, we want the "admin_dashboard" nav link to be highlighted
+    $('#admin_dashboard_link').addClass('highlight_specific_navlink');
+});
