2bbbf95a9a9b3078e6a1590bf922c9a94680b394
amar2468 <plakaloam@gmail.com>
2025-05-03 17:56:04 +0100

    Added the "success" part of the AJAX call, so that the admin knows whether changes were made to the user profile or if no changes were made. Additionally, the admin will be notified if an existing username/email was entered.

--- static/js/admin_edit_user_info.js
+++ static/js/admin_edit_user_info.js
@@ -27,7 +27,55 @@ $(document).ready(function() {
             // If the form was processed correctly in the backend (no errors encountered), we will retrieve the information from the 
             // backend and present it on the page, to inform the admin of the outcome of the operation.
             success: function(response) {
-                console.log(response);
+                // If the user profile was updated, we will notify the admin, and show what fields were updated.
+                if (response.success === true) {
+                    // Clearing the previous messages from the page, as they are now irrelevant.
+                    $("#paragraph_outcome_edit_user_profile_no_changes").text("");
+                    $("#div_outcome_edit_user_profile_no_changes").hide();
+
+                    // Showing the message that informs the admin that the user profile update was successful.
+                    $('#paragraph_outcome_edit_user_profile_success').text(response.message);
+                    $('#div_outcome_edit_user_profile_success').show();
+                }
+                
+                // If the user profile was not updated, we will notify the admin.
+                else if (response.success === false) {
+                    // If no changes were made, we will just display the info on the screen.
+                    if (response.message === "No changes have been made to the user profile.") {
+                        // Clearing the previous messages from the page, as they are now irrelevant.
+                        $("#paragraph_outcome_edit_user_profile_success").text("");
+                        $("#div_outcome_edit_user_profile_success").hide();
+                        
+                        // Showing the message that informs the admin that no changes were made to the user profile.
+                        $('#paragraph_outcome_edit_user_profile_no_changes').text(response.message);
+                        $('#div_outcome_edit_user_profile_no_changes').show();
+                    }
+
+                    // If the admin entered an existing username or email for the user, we will inform the admin that the username 
+                    // or email that they chose already exists on the system.
+                    else {
+                        // Clearing the previous messages from the page, as they are now irrelevant.
+                        $("#paragraph_outcome_edit_user_profile_success").text("");
+                        $("#div_outcome_edit_user_profile_success").hide();
+                        
+                        // Showing the message that informs the admin that no changes were made to the user profile as the username
+                        // already exists on the system.
+                        $('#paragraph_outcome_edit_user_profile_no_changes').text(response.message);
+                        $('#div_outcome_edit_user_profile_no_changes').show();
+
+                        // Changing the "username" input field to use to current username.
+                        $('#edit_username').val($('#current_username').data("url"));
+
+                        // Changing the "email" input field to use the current email
+                        $('#edit_email').val($('#current_email').data("url"));
+                    }
+                }
+                
+                // If something unexpected occurred, we will point that out and get the admin to investigate what happened.
+                else {
+                    $('#paragraph_outcome_edit_user_profile_no_changes').text("Something went wrong - please contact the superadmin for assistance.");
+                    $('#div_outcome_edit_user_profile_no_changes').show();
+                }
             },
 
             // If an error occurred, the error will be logged to the console.
