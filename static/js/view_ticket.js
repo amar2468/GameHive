a563e0ed78aaa0dfbb12683e9ef8b769e9bd145e
amar2468 <plakaloam@gmail.com>
2025-05-28 18:25:53 +0100

    Highlighted the "Admin Dashboard" navlink when on the "view ticket" page + removed backdrop references as it is no longer being used.

--- static/js/view_ticket.js
+++ static/js/view_ticket.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
+
+    // Highlighting the "Admin Dashboard" nav-link, to indicate that we are on that page
+    $('#admin_dashboard_link').addClass('highlight_specific_navlink');
+
     // If the button to toggle the sidebar is clicked, we want to show/hide ticket info
     $(document).on("click", "#toggle_sidebar", function() {
         // If the ticket info is not being displayed, clicking the toggle button will display it.
         if ($('.ticket_info').css("display") === 'none') {
-            $('.custom_backdrop_for_sidebar').removeClass("d-none");
             $('.ticket_info').removeClass("d-none");
         }
 
         // If the ticket info is being displayed, clicking the toggle button will hide it.
         else {
-            $('.custom_backdrop_for_sidebar').addClass("d-none");
             $('.ticket_info').addClass("d-none");
         }
     });
