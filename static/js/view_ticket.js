7c20c1305d32e201b54e035cea1e584a72e4affc
amar2468 <plakaloam@gmail.com>
2025-07-15 17:02:06 +0100

    Added functions for editing the ticket title, saving the changes, and cancelling the changes.

--- static/js/view_ticket.js
+++ static/js/view_ticket.js
@@ -85,4 +85,94 @@ $(document).ready(function() {
 
     });
 
+    // If the button to edit the ticket title is clicked, we will show the input field (where the user can edit the title) and the
+    // save and cancel buttons.
+    $(document).on("click", ".edit_ticket_title", function() {
+        // Getting the current text in the ticket title
+        const ticketTitle = $('#ticket_title').text();
+
+        // Adding the ticket title into the input field and displaying it, so that the user can edit it.
+        $('#ticket_title_input').val(ticketTitle).removeClass("d-none");
+
+        // Hide the ticket title, as the user can now edit the ticket title using the input field instead
+        $('#ticket_title').addClass("d-none");
+
+        // Show the save button
+        $('#save_edited_title').removeClass("d-none");
+
+        // Show the cancel button
+        $('#cancel_edited_title').removeClass("d-none");
+
+        // Hide the pencil icon, which allows the user to edit the ticket title.
+        $(this).addClass("d-none");
+    });
+
+    // If the button to cancel the ticket editing is clicked, it will hide the buttons and the input field, and just
+    // show the ticket title along with the pencil icon for editing the ticket title.
+    $(document).on("click", "#cancel_edited_title", function() {
+        // Hide the cancel button
+        $('#cancel_edited_title').addClass("d-none");
+
+        // Hide the save button
+        $('#save_edited_title').addClass("d-none");
+
+        // Hide the ticket title input field, as the user has finished editing the ticket title
+        $('#ticket_title_input').addClass("d-none");
+
+        // Display the ticket title again
+        $('#ticket_title').removeClass("d-none");
+
+        // Display the button to edit the ticket title again.
+        $('.edit_ticket_title').removeClass("d-none");
+    });
+
+    // When the user clicks on the save button, an attempt will be made to update the ticket title by passing the
+    // new ticket title to the Django view, where it will try to update the record in the model.
+    $(document).on("click", "#save_edited_title", function() {
+        // Retrieving the new ticket title
+        const newTicketTitle = $("#ticket_title_input").val();
+
+        // Retrieving the csrf token from the page, to allow the sending of data through AJAX
+        const csrf_token = $('#csrf_token').val();
+
+        // AJAX POST request that sends the new ticket title and ticket ID to the backend, in order to update the ticket title
+        $.ajax({
+            type: "POST",
+            url: $('#ticket_title_input').data("url"),
+            headers: {
+                'X-CSRFToken': csrf_token
+            },
+            data: {
+                'newTicketTitle': newTicketTitle,
+                'ticketID': $("#ticket_id").val()
+            },
+
+            // If the ticket title updated successfully, we won't do anything. We'll just print the success message
+            // to the console.
+            success: function(response) {
+                console.log(response.message);
+            },
+
+            // If an error occurred, we will display it in the console.
+            error: function(err) {
+                console.error(err);
+            }
+        });
+
+        // Update the ticket title to use the new one.
+        $('#ticket_title').text(newTicketTitle).removeClass("d-none");
+
+        // Hide the save button
+        $('#save_edited_title').addClass("d-none");
+
+        // Hide the cancel button
+        $('#cancel_edited_title').addClass("d-none");
+
+        // Hide the input field that allows you to put the new ticket title
+        $('#ticket_title_input').addClass("d-none");
+
+        // Show the button that allows the editing of the ticket title.
+        $('.edit_ticket_title').removeClass("d-none");
+    });
+
 });
